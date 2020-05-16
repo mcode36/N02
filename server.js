056bd80b57ed6f5ff9c0b452007b54117847e649
Glitch (fate-nine-papyrus) <none>
2020-05-16 18:52:10 +0000

    add register+logout routes

--- server.js
+++ server.js
@@ -105,16 +105,63 @@ client.connect((err, client) => {
     });
   });
 
-  app.route("/login").post(passport.authenticate("local", { failureRedirect: "/" }), (req, res) => {
+  app
+    .route("/login")
+    .post(
+      passport.authenticate("local", { failureRedirect: "/" }),
+      (req, res) => {
         res.redirect("/profile");
-  });
-  
+      }
+    );
+
   app.route("/profile").get(ensureAuthenticated, (req, res) => {
-        res.render(process.cwd() + "/views/pug/profile", {
-          username: req.user.username
-        });
+    res.render(process.cwd() + "/views/pug/profile", {
+      username: req.user.username
+    });
   });
-  
+
+  app.route("/register").post(
+    (req, res, next) => {
+      db.collection("users").findOne({ username: req.body.username }, function(
+        err,
+        user
+      ) {
+        if (err) {
+          next(err);
+        } else if (user) {
+          res.redirect("/");
+        } else {
+          db.collection("users").insertOne(
+            { username: req.body.username, password: req.body.password },
+            (err, doc) => {
+              if (err) {
+                res.redirect("/");
+              } else {
+                next(null, user);
+              }
+            }
+          );
+        }
+      });
+    },
+    passport.authenticate("local", { failureRedirect: "/" }),
+    (req, res, next) => {
+      res.redirect("/profile");
+    }
+  );
+
+  app.route("/logout").get((req, res) => {
+    req.logout();
+    res.redirect("/");
+  });
+
+  app.use((req, res, next) => {
+    res
+      .status(404)
+      .type("text")
+      .send("Not Found");
+  });
+  // spacer
 }); // end_of client.connect
 
 app.get("/test", function(req, res) {
